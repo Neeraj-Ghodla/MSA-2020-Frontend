220e3519a207b04f706a445757773204e9720f22
Neeraj Ghodla <neerajghodla111@gmail.com>
2020-09-17 15:34:43 +1200

    dashboard implemented

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,12 +1,113 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { fetchMovieDetail, likeMovie } from "../../service";
+import Slider from "react-slick";
 
-import { IUser } from "../../service/types";
+import { IData, IDetail, IUser } from "../../service/types";
+import "./Dashboard.css";
 
 interface IDashboardProps {
   user: IUser | undefined;
 }
 
 export default function Dashboard({ user }: IDashboardProps) {
+  const [liked, setLiked] = useState<(IData | undefined)[]>([]);
+  const [disliked, setDisliked] = useState<(IData | undefined)[]>([]);
+  useEffect(() => {
+    if (user) {
+      const { email, likedMovies, dislikedMovies } = user;
+
+      const fetchAPI = async () => {
+        likedMovies.map(async (movieID) => {
+          const d = await fetchMovieDetail(movieID);
+          setLiked((prev) => [...prev, d]);
+        });
+
+        dislikedMovies.map(async (movieID) => {
+          const d = await fetchMovieDetail(movieID);
+          setDisliked((prev) => [...prev, d]);
+        });
+      };
+      fetchAPI();
+    }
+  }, []);
+
   // if (user) const { likedMovies, dislikedMovies } = user;
-  return <div>{user?.email}</div>;
+  if (!user) return null;
+  else {
+    const { email, likedMovies, dislikedMovies } = user;
+    const settings = {
+      infinite: false,
+      speed: 1000,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 992, // width to change options
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            initialSlide: 3,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+      ],
+    };
+
+    const likedMoviesList = liked.map((movie, index) => (
+      <div className="px-3 text-center" key={index}>
+        <div className="card">
+          <Link to={`/movie/${movie?.id}`}>
+            <img
+              className="img-fluid"
+              src={"https://image.tmdb.org/t/p/original/" + movie?.poster_path}
+              alt={movie?.title}
+            />
+          </Link>
+        </div>
+      </div>
+    ));
+
+    const dislikedMoviesList = disliked.map((movie, index) => (
+      <div className="px-3 text-center" key={index}>
+        <div className="card">
+          <Link to={`/movie/${movie?.id}`}>
+            <img
+              className="img-fluid"
+              src={"https://image.tmdb.org/t/p/original/" + movie?.poster_path}
+              alt={movie?.title}
+            />
+          </Link>
+        </div>
+      </div>
+    ));
+
+    return (
+      <div className="container">
+        <div
+          className="row justify-content-center"
+          style={{ flexDirection: "column" }}
+        >
+          <div className="col">
+            <h1 style={{ textAlign: "center" }}>{email}</h1>
+          </div>
+          <div className="col">
+            <h1>Liked Movies</h1>
+            <Slider {...settings}>{likedMoviesList}</Slider>
+          </div>
+          <div className="col">
+            <h1>Disliked Movies</h1>
+            <Slider {...settings}>{dislikedMoviesList}</Slider>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
